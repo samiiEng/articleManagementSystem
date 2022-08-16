27038305db61c14ca9886cfae1c592dc57246d67
samiiEng <mohammad.hossein.samii.iteng@gmail.com>
2022-08-16 23:59:16 +0430

    updateArticle route is done

--- app/Repositories/ArticleRepository.php
+++ app/Repositories/ArticleRepository.php
@@ -218,7 +218,7 @@ class ArticleRepository
 
         event(new StoreArticleEvent($article, $request->messages, $newWaitings));
 
-
+        return "The article is successfully updated!";
     }
 
     public function find($id)
